070a7f9a7acb5082fc133b491c9500c9a2354bbc
Leti <lgsainz15@gmail.com>
2022-02-13 19:10:13 -0500

    cleaning up

--- spotipy/static/js/load.js
+++ spotipy/static/js/load.js
@@ -2,5 +2,5 @@ function load() {
     let loader = document.getElementById('loader');
     loader.className = 'ui massive loader inverted active';
     loader.textContent = '';
-    document.getElementById('wrap').style.pointerEvents = 'none';
+    loader.style.pointerEvents = 'none';
 }
